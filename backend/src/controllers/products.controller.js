776f7742fb11b6c4c2f888b770ab47cc10e5e0c0
Victoria Sales <contatovictoriasales@gmail.com>
2023-06-20 13:07:31 -0300

    Adiciona tratamento de erros (catch)

--- backend/src/controllers/products.controller.js
+++ backend/src/controllers/products.controller.js
@@ -1,32 +1,48 @@
 const productsService = require('../services/products.service');
 
 const getAll = async (_req, res) => {
-  const products = await productsService.getAll();
-  return res.status(200).json(products);
+  try {
+    const products = await productsService.getAll();
+    return res.status(200).json(products);
+  } catch (error) {
+    return res.status(500).json({ message: 'Erro interno', error: error.message });
+  }
 };
 
 const getById = async (req, res) => {
-  const { id } = req.params;
-  const { type, message } = await productsService.getById(id);
-  if (type) {
-    return res.status(404).json({
-      message: 'Product not found',
-    });
+  try {
+    const { id } = req.params;
+    const { type, message } = await productsService.getById(id);
+    if (type) {
+      return res.status(404).json({
+        message: 'Product not found',
+      });
+    }
+    return res.status(200).json(message);
+  } catch (error) {
+    return res.status(500).json({ message: 'Erro interno', error: error.message });
   }
-  return res.status(200).json(message);
 };
 
 const insert = async (req, res) => {
-  const { name } = req.body;
-  const { message } = await productsService.insert(name);
-  return res.status(201).json(message);
+  try {
+    const { name } = req.body;
+    const { message } = await productsService.insert(name);
+    return res.status(201).json(message);
+  } catch (error) {
+    return res.status(500).json({ message: 'Erro interno', error: error.message });
+  }
 };
 
 const update = async (req, res) => {
-  const { id } = req.params;
-  const { name } = req.body;
-  const { type, data } = await productsService.update(id, name);
-  return res.status(type).json(data);
+  try {
+    const { id } = req.params;
+    const { name } = req.body;
+    const { type, data } = await productsService.update(id, name);
+    return res.status(type).json(data);
+  } catch (error) {
+    return res.status(500).json({ message: 'Erro interno', error: error.message });
+  }
 };
 
 module.exports = {
--- backend/src/controllers/sales.controller.js
+++ backend/src/controllers/sales.controller.js
@@ -1,25 +1,37 @@
 const salesService = require('../services/sales.service');
 
 const getAll = async (_req, res) => {
-  const sales = await salesService.getAll();
-  return res.status(200).json(sales);
+  try {
+    const sales = await salesService.getAll();
+    return res.status(200).json(sales);
+  } catch (error) {
+    return res.status(500).json({ message: 'Erro interno', error: error.message });
+  }
 };
 
 const getById = async (req, res) => {
-  const { id } = req.params;
-  const sale = await salesService.getById(id);
-  if (sale.length === 0) {
-    return res.status(404).json({
-      message: 'Sale not found',
-    });
+  try {
+    const { id } = req.params;
+    const sale = await salesService.getById(id);
+    if (sale.length === 0) {
+      return res.status(404).json({
+        message: 'Sale not found',
+      });
+    }
+    return res.status(200).json(sale);
+  } catch (error) {
+    return res.status(500).json({ message: 'Erro interno', error: error.message });
   }
-  return res.status(200).json(sale);
 };
 
 const insert = async (req, res) => {
-  const sale = req.body;
-  const { message } = await salesService.insert(sale);
-  return res.status(201).json(message);
+  try {
+    const sale = req.body;
+    const { message } = await salesService.insert(sale);
+    return res.status(201).json(message);
+  } catch (error) {
+    return res.status(500).json({ message: 'Erro interno', error: error.message });
+  }
 };
 
 module.exports = {
